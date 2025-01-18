0ecfcd55de93cf8698562f5ec8e7c8eddc5a6807
Jakub Dereszewski <kuba6356@gmail.com>
2025-01-18 17:29:48 +0100

    changed the path for the server

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join()));
 
 // Start the server
 const port = 3000;
